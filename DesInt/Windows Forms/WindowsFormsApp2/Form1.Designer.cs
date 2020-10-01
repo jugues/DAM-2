cc8ea765a3ac4e3c71b99b9d829acb2136e9cb95
Juli G <55200452+jugues@users.noreply.github.com>
2020-10-01 11:52:04 +0200

    Add files via upload

---
+++ DesInt/Windows Forms/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnPulsa = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // BtnPulsa
+            // 
+            this.BtnPulsa.Location = new System.Drawing.Point(301, 186);
+            this.BtnPulsa.Name = "BtnPulsa";
+            this.BtnPulsa.Size = new System.Drawing.Size(75, 23);
+            this.BtnPulsa.TabIndex = 1;
+            this.BtnPulsa.Text = "Pulsa";
+            this.BtnPulsa.UseVisualStyleBackColor = true;
+            this.BtnPulsa.Click += new System.EventHandler(this.BtnPulsa_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Pulsa 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnPulsa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 563);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnPulsa);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnPulsa;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DesInt/Windows Forms/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPulsa_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+
+            if(boton.Name.Equals("BtnPulsa"))
+            {
+                textBox1.Text = "Hola, mundo";
+            }
+
+            else
+            {
+                textBox1.Text = "Hola, mundo2";
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

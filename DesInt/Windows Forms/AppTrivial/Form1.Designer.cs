06e37af81fa4fe00842d2cfecba1f02256858fc2
Juli G <55200452+jugues@users.noreply.github.com>
2020-10-11 23:15:03 +0200

    Add files via upload

---
+++ DesInt/Windows Forms/AppTrivial/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace AppTrivial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.txtCorreccion = new System.Windows.Forms.TextBox();
+            this.txtOpcion1 = new System.Windows.Forms.TextBox();
+            this.txtOpcion2 = new System.Windows.Forms.TextBox();
+            this.txtOpcion3 = new System.Windows.Forms.TextBox();
+            this.txtOpcion4 = new System.Windows.Forms.TextBox();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.partidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nombreCapitalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nombrePaísesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.múltiplesOpcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.escribirRespuestaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "País:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Capital:";
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(73, 64);
+            this.txtRespuesta.Multiline = true;
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(259, 46);
+            this.txtRespuesta.TabIndex = 2;
+            // 
+            // txtCorreccion
+            // 
+            this.txtCorreccion.Location = new System.Drawing.Point(122, 142);
+            this.txtCorreccion.Multiline = true;
+            this.txtCorreccion.Name = "txtCorreccion";
+            this.txtCorreccion.Size = new System.Drawing.Size(156, 45);
+            this.txtCorreccion.TabIndex = 3;
+            // 
+            // txtOpcion1
+            // 
+            this.txtOpcion1.Location = new System.Drawing.Point(54, 250);
+            this.txtOpcion1.Multiline = true;
+            this.txtOpcion1.Name = "txtOpcion1";
+            this.txtOpcion1.Size = new System.Drawing.Size(278, 46);
+            this.txtOpcion1.TabIndex = 4;
+            // 
+            // txtOpcion2
+            // 
+            this.txtOpcion2.Location = new System.Drawing.Point(54, 318);
+            this.txtOpcion2.Multiline = true;
+            this.txtOpcion2.Name = "txtOpcion2";
+            this.txtOpcion2.Size = new System.Drawing.Size(278, 46);
+            this.txtOpcion2.TabIndex = 5;
+            // 
+            // txtOpcion3
+            // 
+            this.txtOpcion3.Location = new System.Drawing.Point(54, 385);
+            this.txtOpcion3.Multiline = true;
+            this.txtOpcion3.Name = "txtOpcion3";
+            this.txtOpcion3.Size = new System.Drawing.Size(278, 46);
+            this.txtOpcion3.TabIndex = 6;
+            // 
+            // txtOpcion4
+            // 
+            this.txtOpcion4.Location = new System.Drawing.Point(54, 458);
+            this.txtOpcion4.Multiline = true;
+            this.txtOpcion4.Name = "txtOpcion4";
+            this.txtOpcion4.Size = new System.Drawing.Size(278, 46);
+            this.txtOpcion4.TabIndex = 7;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(73, 543);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(121, 34);
+            this.btnSiguiente.TabIndex = 8;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(73, 583);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(121, 34);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Location = new System.Drawing.Point(238, 565);
+            this.txtPorcentaje.Multiline = true;
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(62, 38);
+            this.txtPorcentaje.TabIndex = 10;
+            // 
+            // partidaToolStripMenuItem
+            // 
+            this.partidaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.salirToolStripMenuItem});
+            this.partidaToolStripMenuItem.Name = "partidaToolStripMenuItem";
+            this.partidaToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.partidaToolStripMenuItem.Text = "Partida";
+            // 
+            // nuevaToolStripMenuItem
+            // 
+            this.nuevaToolStripMenuItem.Name = "nuevaToolStripMenuItem";
+            this.nuevaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevaToolStripMenuItem.Text = "Nueva";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nombreCapitalesToolStripMenuItem,
+            this.nombrePaísesToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.múltiplesOpcionesToolStripMenuItem,
+            this.escribirRespuestaToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // nombreCapitalesToolStripMenuItem
+            // 
+            this.nombreCapitalesToolStripMenuItem.Name = "nombreCapitalesToolStripMenuItem";
+            this.nombreCapitalesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nombreCapitalesToolStripMenuItem.Text = "Nombre Grupos";
+            // 
+            // nombrePaísesToolStripMenuItem
+            // 
+            this.nombrePaísesToolStripMenuItem.Name = "nombrePaísesToolStripMenuItem";
+            this.nombrePaísesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nombrePaísesToolStripMenuItem.Text = "Nombre Libros";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // múltiplesOpcionesToolStripMenuItem
+            // 
+            this.múltiplesOpcionesToolStripMenuItem.CheckOnClick = true;
+            this.múltiplesOpcionesToolStripMenuItem.Name = "múltiplesOpcionesToolStripMenuItem";
+            this.múltiplesOpcionesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.múltiplesOpcionesToolStripMenuItem.Text = "Múltiples Opciones";
+            // 
+            // escribirRespuestaToolStripMenuItem
+            // 
+            this.escribirRespuestaToolStripMenuItem.CheckOnClick = true;
+            this.escribirRespuestaToolStripMenuItem.Name = "escribirRespuestaToolStripMenuItem";
+            this.escribirRespuestaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.escribirRespuestaToolStripMenuItem.Text = "Escribir Respuesta";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.partidaToolStripMenuItem,
+            this.opcionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(382, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 663);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.txtOpcion4);
+            this.Controls.Add(this.txtOpcion3);
+            this.Controls.Add(this.txtOpcion2);
+            this.Controls.Add(this.txtOpcion1);
+            this.Controls.Add(this.txtCorreccion);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Trivial";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.TextBox txtCorreccion;
+        private System.Windows.Forms.TextBox txtOpcion1;
+        private System.Windows.Forms.TextBox txtOpcion2;
+        private System.Windows.Forms.TextBox txtOpcion3;
+        private System.Windows.Forms.TextBox txtOpcion4;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.ToolStripMenuItem partidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nombreCapitalesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nombrePaísesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem múltiplesOpcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem escribirRespuestaToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ DesInt/Windows Forms/AppTrivial/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTrivial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String opcion;
+        int numRandom;
+        int porcentaje;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /* 
+         Por hacer: eventos, modo grupos de música y modo libros, colores.
+         */
+    }
+}

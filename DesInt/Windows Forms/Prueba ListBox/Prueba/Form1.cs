c84ef8b1633822afee8a6b34b2384097048b9f93
Juli G <55200452+jugues@users.noreply.github.com>
2020-10-18 22:31:35 +0200

    Add files via upload

---
+++ DesInt/Windows Forms/Prueba ListBox/Prueba/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Segundo");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = string.Empty;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+                label1.Text = (string)listBox1.Items[listBox1.SelectedIndex];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int indice = listBox1.SelectedIndex;
+
+            if(indice != -1)
+            {
+                listBox1.Items.RemoveAt(indice);
+                label1.Text = "";
+            }
+        }
+    }
+}

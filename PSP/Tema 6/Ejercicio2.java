a2eba975498bc01a1ff9730df1164c2e4012a489
Juli G <55200452+jugues@users.noreply.github.com>
2020-10-22 19:56:32 +0200

    Add files via upload

--- PSP/Tema 6/Ejercicio2.java
+++ PSP/Tema 6/Ejercicio2.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Ejercicio2 {
 	
@@ -6,7 +8,7 @@ public class Ejercicio2 {
     PrintWriter pw = null;
     BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     String nombre, dni, email;
-    int numTelf;
+    String numTelf;
     
     public Ejercicio2()
     {
@@ -20,21 +22,57 @@ public class Ejercicio2 {
         	dni = teclado.readLine();
         	System.out.println("Ahora un email válido");
         	email = teclado.readLine();
-        	System.out.println("Finalmente, necesitamos un número de teléfono:");
-        	numTelf = teclado.read();
+        	System.out.println("Finalmente, necesitamos un número de teléfono (9 números sin prefijo de país):");
+        	numTelf = teclado.readLine();
+        	
+        	//patrones de todo lo anterior, comprobar si son correctos los datos introducidos, lanzar excepciones si no es así
+        	
+        	//Comprobar nombre 
+        	String comprobarNombre = "[a-zA-Z]{1,}";
+        	Pattern patternNombre = Pattern.compile(comprobarNombre); 
+        	Matcher matcherNombre = patternNombre.matcher(nombre);
+        	boolean matchesNombre = matcherNombre.matches();
+        	
+        	//comprobar dni
+        	String comprobarDni = "\\d{8}[A-HJ-NP-TV-Z]"; 
+        	Pattern patternDni = Pattern.compile(comprobarDni); 
+        	Matcher matcherDni = patternDni.matcher(dni); 
+        	boolean matchesDni = matcherDni.matches();
+        	
+        	//comprobar teléfono
+        	String comprobarTelf = "\\d{9}";
+        	Pattern patternTelf = Pattern.compile(comprobarTelf);
+        	Matcher matcherTelf = patternTelf.matcher(numTelf);
+        	boolean matchesTelf = matcherTelf.matches();
+        	//comprobar formato del email
+        	String comprobarEmail = "[^@]+@[^@]+\\.[a-zA-Z]{2,}";
+        	Pattern patternEmail = Pattern.compile(comprobarEmail);
+        	Matcher matcherEmail = patternEmail.matcher(email);
+        	boolean matchesEmail = matcherEmail.matches();
+        	
+        	if(matchesNombre && matchesDni && matchesTelf && matchesEmail)
+        	{
+        		System.out.println("Todos los datos son correctos.");
+        	}
+        	
+        	else
+        	{
+        		throw new Exception();
+        	}
         	
     	}
     	
     	catch(Exception e)
     	{
+    		System.out.println("Alguno de los datos introducidos no es correcto. Inténtalo de nuevo.");
     		e.printStackTrace();
     	}
-    	
+
     }
 
 	public static void main(String[] args) {
 		
-		
+		new Ejercicio2();
 
 	}
 

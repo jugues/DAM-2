4f1a87f29d2a1caedf88dc8a45d87535aab39b95
Juli G <55200452+jugues@users.noreply.github.com>
2020-10-26 20:37:42 +0100

    Add files via upload

---
+++ DesInt/Windows Forms/HerenciaPrueba/FrmInicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HerenciaPrueba
+{
+    public partial class FrmInicio : Form
+    {
+
+        //internal static ArrayList list = new ArrayList();
+        internal static SortedList <String, Cliente> mySortedList = new SortedList<String, Cliente>();
+        public FrmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmInicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void altaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAlta f = new FrmAlta();
+            Hide();
+            f.Show();
+            f.WindowState = FormWindowState.Maximized;
+            //f.Text = "¡Hola!";
+        }
+    }
+}

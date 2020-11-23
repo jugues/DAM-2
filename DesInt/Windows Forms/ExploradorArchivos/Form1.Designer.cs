273d94c3c2de892676a52e7da520541042bf3247
Juli G <55200452+jugues@users.noreply.github.com>
2020-11-23 21:21:47 +0100

    Add files via upload

--- DesInt/Windows Forms/ExploradorArchivos/Form1.Designer.cs
+++ DesInt/Windows Forms/ExploradorArchivos/Form1.Designer.cs
@@ -38,7 +38,9 @@
             this.treeviewDevices = new System.Windows.Forms.TreeView();
             this.imageList = new System.Windows.Forms.ImageList(this.components);
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.carpetaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ficheroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,9 +52,9 @@
             this.cModificacion,
             this.cAcceso});
             this.listviewFiles.HideSelection = false;
-            this.listviewFiles.Location = new System.Drawing.Point(351, 193);
+            this.listviewFiles.Location = new System.Drawing.Point(345, 193);
             this.listviewFiles.Name = "listviewFiles";
-            this.listviewFiles.Size = new System.Drawing.Size(379, 392);
+            this.listviewFiles.Size = new System.Drawing.Size(412, 392);
             this.listviewFiles.TabIndex = 0;
             this.listviewFiles.UseCompatibleStateImageBehavior = false;
             this.listviewFiles.View = System.Windows.Forms.View.Details;
@@ -83,7 +85,7 @@
             this.treeviewDevices.Location = new System.Drawing.Point(32, 193);
             this.treeviewDevices.Name = "treeviewDevices";
             this.treeviewDevices.SelectedImageIndex = 0;
-            this.treeviewDevices.Size = new System.Drawing.Size(247, 392);
+            this.treeviewDevices.Size = new System.Drawing.Size(281, 392);
             this.treeviewDevices.TabIndex = 1;
             this.treeviewDevices.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeviewDevices_AfterSelect);
             // 
@@ -97,22 +99,36 @@
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripDropDownButton1});
+            this.toolStripSplitButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(769, 25);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripDropDownButton1
+            // toolStripSplitButton1
             // 
-            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
-            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
-            this.toolStripDropDownButton1.Text = "Nuevo";
-            this.toolStripDropDownButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carpetaToolStripMenuItem,
+            this.ficheroToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 22);
+            this.toolStripSplitButton1.Text = "Nuevo";
+            // 
+            // carpetaToolStripMenuItem
+            // 
+            this.carpetaToolStripMenuItem.Name = "carpetaToolStripMenuItem";
+            this.carpetaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.carpetaToolStripMenuItem.Text = "Carpeta";
+            // 
+            // ficheroToolStripMenuItem
+            // 
+            this.ficheroToolStripMenuItem.Name = "ficheroToolStripMenuItem";
+            this.ficheroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ficheroToolStripMenuItem.Text = "Fichero";
             // 
             // Form1
             // 
@@ -142,7 +158,9 @@
         private System.Windows.Forms.ColumnHeader cAcceso;
         private System.Windows.Forms.ImageList imageList;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem carpetaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ficheroToolStripMenuItem;
     }
 }
 
--- DesInt/Windows Forms/ExploradorArchivos/Form1.cs
+++ DesInt/Windows Forms/ExploradorArchivos/Form1.cs
@@ -20,33 +20,63 @@ namespace ExploradorArchivos
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //cargamos las unidades y les ponemos el icono correspondiente
-            String[] drives = Directory.GetLogicalDrives();
-            foreach(String u in drives)
-            {
-                TreeNode n = treeviewDevices.Nodes.Add(u);
-                n.ImageIndex = 0;
-                n.SelectedImageIndex = 0;
-            }
+            loadFolders();
         }
 
         private void treeviewDevices_AfterSelect(object sender, TreeViewEventArgs e)
         {
             //meter los hijos de cada nodo
 
-            String [] directories = Directory.GetDirectories(e.Node.Text); //directorio del nodo pinchado
-            foreach (String d in directories)
+
+            try
             {
-                //String sd = d; 
-                //sd = sd.Substring(0, sd.LastIndexOf('/'));
-                TreeNode t = e.Node.Nodes.Add(d);
-                t.ImageIndex = 1;
-                t.SelectedImageIndex = 1;
-                t.Tag = d;
+                String[] directories = Directory.GetDirectories(e.Node.Text); //directorio del nodo pinchado
+                foreach (String d in directories)
+                {
+                    //String sd = d; 
+                    //sd = sd.Substring(0, sd.LastIndexOf('/'));
+                    TreeNode t = e.Node.Nodes.Add(d);
+                    t.ImageIndex = 1;
+                    t.SelectedImageIndex = 1;
+                    t.Tag = d;
+                    listviewFiles.Items.Clear();
+                    listviewFiles.Refresh();
+                }
+
+                //mostrar archivos de las carpetas
+                DirectoryInfo dInfo = new DirectoryInfo((string)e.Node.Text);
+                foreach (FileInfo f in dInfo.GetFiles())
+                {
+                    //var created = f.CreationTime;
+                    //var lastModified = f.LastWriteTime;
+                    ListViewItem lv = listviewFiles.Items.Add(f.Name);
+                    lv.SubItems.Add(f.CreationTime.ToString());
+                    lv.SubItems.Add(f.LastWriteTime.ToString());
+                    lv.SubItems.Add(f.LastAccessTime.ToString());
+
+                }
+
+                //mostrar los ficheros en el listview
+                String[] files = Directory.GetFiles(e.Node.Text);
             }
 
-            //mostrar los ficheros en el listview
-            String[] files = Directory.GetFiles(e.Node.Text);
+            catch (Exception ex)
+            {
+                MessageBox.Show("You don't have permission to access here.");
+            }
+            
+        }
+
+        public void loadFolders()
+        {
+            //cargamos las unidades y les ponemos el icono correspondiente
+            String[] drives = Directory.GetLogicalDrives();
+            foreach (String u in drives)
+            {
+                TreeNode n = treeviewDevices.Nodes.Add(u);
+                n.ImageIndex = 0;
+                n.SelectedImageIndex = 0;
+            }
         }
     }
 }

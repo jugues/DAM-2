026266ab874d7d22726ea8abb43f00a7ce504240
Juli G <55200452+julito97@users.noreply.github.com>
2021-01-25 21:05:59 +0100

    Add files via upload

---
+++ DesInt/Windows Forms/Conexion Nwind/Inicio.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Conexion_Emple
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxProductos = new System.Windows.Forms.ListBox();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.btnConsultaFrank = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listboxProductos
+            // 
+            this.listboxProductos.FormattingEnabled = true;
+            this.listboxProductos.Location = new System.Drawing.Point(64, 55);
+            this.listboxProductos.Name = "listboxProductos";
+            this.listboxProductos.Size = new System.Drawing.Size(198, 264);
+            this.listboxProductos.TabIndex = 0;
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(307, 72);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(124, 44);
+            this.btnConsulta.TabIndex = 1;
+            this.btnConsulta.Text = "Consultar productos";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // btnConsultaFrank
+            // 
+            this.btnConsultaFrank.Location = new System.Drawing.Point(307, 122);
+            this.btnConsultaFrank.Name = "btnConsultaFrank";
+            this.btnConsultaFrank.Size = new System.Drawing.Size(124, 44);
+            this.btnConsultaFrank.TabIndex = 2;
+            this.btnConsultaFrank.Text = "Consultar Frank";
+            this.btnConsultaFrank.UseVisualStyleBackColor = true;
+            this.btnConsultaFrank.Click += new System.EventHandler(this.btnConsultaFrank_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 501);
+            this.Controls.Add(this.btnConsultaFrank);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.listboxProductos);
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listboxProductos;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.Button btnConsultaFrank;
+    }
+}
+
---
+++ DesInt/Windows Forms/Conexion Nwind/Inicio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Conexion_Emple
+{
+    public partial class Inicio : Form
+    {
+        OleDbConnection ctn;
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            ctn = new OleDbConnection();
+            ctn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; " + 
+                "Data Source=C:\\Users\\Juli\\Desktop\\Archivos\\Interfaces Bases\\Nwind.mdb";
+            ctn.Open();
+            MessageBox.Show(ctn.State.ToString());
+
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+            IDbCommand cmd = ctn.CreateCommand(); 
+            cmd.CommandText = "select * from products";
+            IDataReader reader = cmd.ExecuteReader(); //interfaz válida para cualquier tipo de conexión
+
+            while(reader.Read()) //mientras queden registros por leer
+            {
+                this.listboxProductos.Items.Add(reader.GetString(1)); //cogemos la segunda columna del datareader
+            }
+        }
+
+        private void btnConsultaFrank_Click(object sender, EventArgs e) //otra forma de consultar
+        {
+            OleDbCommand cmd;
+            cmd = new OleDbCommand();
+            cmd.Connection = ctn;
+            cmd.CommandText = "select count(orderid) from orders where customerid = 'FRANK'";
+            MessageBox.Show(cmd.ExecuteScalar().ToString()); //Scalar es eficiente para números
+        }
+    }
+}

02e5e7a74cf91c0c8de0ca84036d2748cfcd1d8a
Juli G <55200452+julito97@users.noreply.github.com>
2021-02-07 19:45:43 +0100

    Add files via upload

---
+++ DesInt/Mis Apps/PruebaDragDrop/FrmPrincipal.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace PruebaDragDrop
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.AllowDrop = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(172, 68);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(181, 304);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.checkedListBox1_MouseDown);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.AllowDrop = true;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(409, 68);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(181, 304);
+            this.checkedListBox2.TabIndex = 1;
+            this.checkedListBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.checkedListBox2_DragDrop);
+            this.checkedListBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.checkedListBox2_DragEnter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 303);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 526);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "FrmPrincipal";
+            this.Text = "App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DesInt/Mis Apps/PruebaDragDrop/FrmPrincipal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaDragDrop
+{
+    public partial class FrmPrincipal : Form
+    {
+        int index = 0;
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            string[] items1 = {"Ranma 1/2", "Inuyasha", "Maison Ikkoku"};
+            checkedListBox1.Items.AddRange(items1);
+        }
+
+        private void checkedListBox1_MouseDown(object sender, MouseEventArgs e) //efecto del drag and drop
+        {
+            CheckedListBox chk1 = sender as CheckedListBox;
+            index = chk1.IndexFromPoint(e.X, e.Y);
+
+            if(index >= 0 && e.Button == MouseButtons.Left)
+            {
+                chk1.DoDragDrop(chk1.Items[index].ToString(), DragDropEffects.Move);
+            }
+        }
+
+        private void checkedListBox2_DragDrop(object sender, DragEventArgs e) //sobre el control que recibe los datos
+        {
+            CheckedListBox chk2 = sender as CheckedListBox;
+            chk2.Items.Add(e.Data.GetData(typeof(System.String)).ToString());
+            checkedListBox1.Items.RemoveAt(index);
+        }
+
+        private void checkedListBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /* private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            ListBox listBox1 = sender as ListBox;
+            listBox1.Items.Add(e.Data.GetData(typeof(System.String)).ToString());
+            checkedListBox1.Items.RemoveAt(index);
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    } */
+}
